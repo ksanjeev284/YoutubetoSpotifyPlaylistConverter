9dceece8b7a02b74e0b2bf642a16871a5428fecb
Sanjeev Kumar <ksanjeev284@gmail.com>
2024-11-26 02:44:04 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ConversionResult, Track, YouTubePlaylist } from '../types';
 
-const API_URL = import.meta.env.PROD ? '/.netlify/functions' : 'http://localhost:8888/.netlify/functions';
+const API_URL = import.meta.env.PROD ? '/.netlify/functions' : 'https://youtubetospotifyplaylistconverter.netlify.app/.netlify/functions/spotify-auth';
 
 export async function fetchYouTubePlaylist(url: string): Promise<YouTubePlaylist> {
   const response = await axios.post(`${API_URL}/youtube-playlist`, { url });
